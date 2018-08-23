910cecdf510f96287c2d923b55bf0c2fd88e7610
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-23 18:11:02 +0800

    Capitalize keypads and keypad IDs

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ class App extends Component {
         <table className="table key-pad">
           <tbody>
             <tr>
-              <td className="drum-pad" id="q"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip"></audio>q</td>
-              <td className="drum-pad" id="w"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" className="clip"></audio>w</td>
-              <td className="drum-pad" id="e"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" className="clip" ></audio>e</td>
+              <td className="drum-pad" id="Q"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip"></audio>Q</td>
+              <td className="drum-pad" id="W"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" className="clip"></audio>W</td>
+              <td className="drum-pad" id="E"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" className="clip" ></audio>E</td>
             </tr>
             <tr>
-              <td className="drum-pad" id="a"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" className="clip"></audio>a</td>
-              <td className="drum-pad" id="s"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" className="clip" ></audio>s</td>
-              <td className="drum-pad" id="d"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" className="clip" ></audio>d</td>
+              <td className="drum-pad" id="A"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" className="clip"></audio>A</td>
+              <td className="drum-pad" id="S"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" className="clip" ></audio>S</td>
+              <td className="drum-pad" id="D"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" className="clip" ></audio>D</td>
             </tr>
             <tr>
-              <td className="drum-pad" id="z"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" className="clip"></audio>z</td>
-              <td className="drum-pad" id="x"><audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" className="clip"></audio>x</td>
-              <td className="drum-pad" id="c"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" className="clip"></audio>c</td>
+              <td className="drum-pad" id="Z"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" className="clip"></audio>Z</td>
+              <td className="drum-pad" id="X"><audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" className="clip"></audio>X</td>
+              <td className="drum-pad" id="C"><audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" className="clip"></audio>C</td>
             </tr>
           </tbody>
 
